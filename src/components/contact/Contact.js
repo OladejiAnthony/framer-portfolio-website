6d183a3125c5023fdf8dd83893c7bf3721ab67d5
Oladeji Anthony <oladejianthony98@gmail.com>
2023-11-28 12:12:20 +0100

    portfolio page complete

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,10 +5,27 @@ function Contact() {
   return (
     <div className='contact'>
       <div className='textContainer'>
-
+        <h1>Let's work together</h1>
+        <div className='item'>
+            <h2>Mail</h2>
+            <span>hello@react.dev</span>
+        </div>
+        <div className='item'>
+            <h2>Address</h2>
+            <span>21 Taiwo Road, Kwara State Nigeria</span>
+        </div>
+        <div className='item'>
+            <h2>Phone</h2>
+            <span>+234 813 745 9380</span>
+        </div>
       </div>
       <div className='formContainer'>
-
+        <form>
+            <input type='text' required placeholder='Name' />
+            <input type='email' required placeholder='Email' />
+            <textarea rows={8} placeholder='Message' />
+            <button>Submit</button>
+        </form>
       </div>
     </div>
   )
